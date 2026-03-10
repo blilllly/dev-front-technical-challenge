33ad9cefe3cf6fd748c8d54832de8b3cc2a658ac
Billy <alvearbilly@gmail.com>
2026-03-10 18:00:43 -0500

    form

--- src/app/bank-front/pages/form-page/form-page.ts
+++ src/app/bank-front/pages/form-page/form-page.ts
@@ -1,10 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-form-page',
-  imports: [],
+  selector: 'form-page',
+  imports: [ReactiveFormsModule],
   templateUrl: './form-page.html',
   styleUrl: './form-page.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FormPage { }
+export class FormPage {
+  private fb = inject(FormBuilder);
+
+  myForm = this.fb.group({
+    id: ['', Validators.required, Validators.minLength(3), Validators.maxLength(10)],
+    name: ['', Validators.required, Validators.minLength(5), Validators.maxLength(100)],
+  })
+}
